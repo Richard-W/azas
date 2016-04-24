eff65a560ca5380f6fd555cb6995a0d914425088
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2016-04-24 12:42:51 +0200

    Change input type of password field

--- src/main/assets/modules/azas/password.component.ts
+++ src/main/assets/modules/azas/password.component.ts
@@ -5,7 +5,7 @@ import {Component, Output, EventEmitter} from 'angular2/core';
 	template:`
 	<form>
 	<label for="token">Master password:</label><br />
-	<input type="text" [(ngModel)]="pass" />
+	<input type="password" [(ngModel)]="pass" />
 	<button (click)="onPasswordSubmit()">Abschicken</button>
 	</form>
 	`
